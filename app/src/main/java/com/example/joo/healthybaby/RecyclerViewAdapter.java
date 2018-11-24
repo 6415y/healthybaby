8e3032de6ad8e4ab2061468245cbc3628fdc8333
lolhi <lolhi@naver.com>
2018-11-24 12:03:28 +0900

    VaccinActivity: 예방접종 횟수 이미지로 추가

--- app/src/main/java/com/example/joo/healthybaby/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/joo/healthybaby/RecyclerViewAdapter.java
@@ -14,38 +14,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    /*
-     *  public static class RecyclerViewHolder extends RecyclerView.ViewHolder
-     *
-     *  RecyclerView와 연결된 recycler_view_row.xml의 변수들 생성 및 초기화
-     */
-    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
-        TextView tvVaccinName;
-
-        //Test중인 코드...
-        ImageView[] injection_iv = new ImageView[5];
-
-        public RecyclerViewHolder(View itemView) {
-            super(itemView);
-            tvVaccinName = itemView.findViewById(R.id.vaccinName_tv);
-            /*Test중인 코드...
-            injection_iv[0] = itemView.findViewById(R.id.injectionLeft_iv_recyclerViewRow);
-            injection_iv[1] = itemView.findViewById(R.id.injectionMiddle1_iv_recyclerViewRow);
-            injection_iv[2] = itemView.findViewById(R.id.injectionMiddle2_iv_recyclerViewRow);
-            injection_iv[3] = itemView.findViewById(R.id.injectionMiddle3_iv_recyclerViewRow);
-            injection_iv[4] = itemView.findViewById(R.id.injectionRight_iv_recyclerViewRow);
-            */
-        }
-    }
-
     private ArrayList<VaccinInfo> vaccinInfoArrayList;
-    private int count = 4;
-
     /*
      *  private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
      *  private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -54,7 +29,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
      */
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
-
 
     public RecyclerViewAdapter(ArrayList<VaccinInfo> vaccinInfoArrayList) {
         this.vaccinInfoArrayList = vaccinInfoArrayList;
@@ -86,46 +60,53 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
          *  RecyclerView item 의 tvVaccinName을 백신의 이름으로 설정
          */
         recyclerViewHolder.tvVaccinName.setText(vaccinInfoArrayList.get(position).getVaccinName());
-        /* Test중인 코드...
+
         for(int i = 0; i < recyclerViewHolder.injection_iv.length - vaccinInfoArrayList.get(position).getInoculateDate().length; i++)
             recyclerViewHolder.injection_iv[i].setVisibility(View.INVISIBLE);
-        databaseReference.child("InoculateResult").addChildEventListener(new ChildEventListener() {
+
+        databaseReference.child("InoculateResult").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                InoculateInfo info = dataSnapshot.getValue(InoculateInfo.class);
-                if(info.getVaccinName().contains(vaccinInfoArrayList.get(position).getVaccinName())){
-                    recyclerViewHolder.injection_iv[count].setImageResource(R.drawable.injection);
-                    count--;
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int count = 4;
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    InoculateInfo info = snapshot.getValue(InoculateInfo.class);
+                    if (info.getVaccinName().contains(vaccinInfoArrayList.get(position).getVaccinName())) {
+                        recyclerViewHolder.injection_iv[count].setImageResource(R.drawable.injection);
+                        count--;
+                    }
                 }
-
-
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
-        */
     }
 
     @Override
     public int getItemCount() {
         return vaccinInfoArrayList.size();
+    }
+
+    /*
+     *  public static class RecyclerViewHolder extends RecyclerView.ViewHolder
+     *
+     *  RecyclerView와 연결된 recycler_view_row.xml의 변수들 생성 및 초기화
+     */
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView tvVaccinName;
+
+        ImageView[] injection_iv = new ImageView[5];
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            tvVaccinName = itemView.findViewById(R.id.vaccinName_tv);
+
+            injection_iv[0] = itemView.findViewById(R.id.injectionLeft_iv_recyclerViewRow);
+            injection_iv[1] = itemView.findViewById(R.id.injectionMiddle1_iv_recyclerViewRow);
+            injection_iv[2] = itemView.findViewById(R.id.injectionMiddle2_iv_recyclerViewRow);
+            injection_iv[3] = itemView.findViewById(R.id.injectionMiddle3_iv_recyclerViewRow);
+            injection_iv[4] = itemView.findViewById(R.id.injectionRight_iv_recyclerViewRow);
+        }
     }
 }
