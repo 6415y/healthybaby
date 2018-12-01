9d59d224a168260442d472ccac2fceff7ac6958c
lolhi <lolhi@naver.com>
2018-12-01 23:06:43 +0900

    MainActivity: 식단기록버튼 이름변경 및 불러오는 Activity 변경

--- app/src/main/java/com/example/joo/healthybaby/MainActivity.java
+++ app/src/main/java/com/example/joo/healthybaby/MainActivity.java
@@ -207,7 +207,7 @@ public class MainActivity extends AppCompatActivity {
         dietbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, DietRecordActivity.class);
+                Intent intent = new Intent(MainActivity.this, AllergyActivity.class);
                 startActivity(intent);
             }
         });
